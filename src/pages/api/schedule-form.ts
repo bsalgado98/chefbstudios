bd43045c31e1fb895537920038feb9128d0f5e47
Bruno Salgado <bsalgado@stevens.edu>
2023-04-14 01:42:14 -0400

    adding email sending capability to schedule view

--- src/pages/api/schedule-form.ts
+++ src/pages/api/schedule-form.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import nodemailer from 'nodemailer'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     const body = req.body
@@ -18,6 +19,27 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (!body.message) {
         return res.status(400).json({ data: 'Message not found!' })
     }
+
+    const transporter = nodemailer.createTransport({
+        host: 'email-smtp.us-east-1.amazonaws.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.NEXT_PUBLIC_AWS_SMTP_USER,
+            pass: process.env.NEXT_PUBLIC_AWS_SMTP_PWD
+        }
+    })
+    const mailOptions = {
+        from: 'brunosalgado98@gmail.com',
+        to: 'brunosalgado98@gmail.com',
+        subject: body.subject,
+        text: body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) console.error(error)
+        console.log('nodemailer sent info:', info)
+    })
 
     res.status(200).json({ data: `Name: ${body.name}; Email: ${body.email}; Subject: ${body.subject}; Message: ${body.message};`})
 }
